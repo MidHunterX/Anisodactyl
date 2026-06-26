2281de514f8daebefa7494dcdbda9b188e5af97e
MidHunterX <midhunterx@gmail.com>
2026-06-27 04:53:29 +0530

    ✅ test: add protocol adherence test

---
+++ tests/test_protocols.py
@@ -0,0 +1,28 @@
+from anisodactyl.crud._protocols import CRUDProtocol
+from anisodactyl.crud.sqlalchemy import CRUDBase
+from anisodactyl.query._protocols import QueryParserProtocol
+from anisodactyl.query.anisodactyl import QueryParams
+from anisodactyl.query.django import DjangoQueryParams
+from tests.mocks import Model, create_mock_request
+
+
+class TestProtocolConformance:
+    """Test that implementations conform to protocols."""
+
+    def test_crud_implements_protocol(self):
+        """CRUD implementation should be recognized as a CRUDProtocol."""
+        # Create instance with minimal requirements
+        crud_instance = CRUDBase(model=Model)
+        assert isinstance(crud_instance, CRUDProtocol)
+
+    def test_anisodactyl_query_implements_protocol(self):
+        """Anisodactyl QueryParser implements QueryParserProtocol."""
+        request_mock = create_mock_request("name=alice&status=eq:active")
+        parser = QueryParams(request_mock)
+        assert isinstance(parser, QueryParserProtocol)
+
+    def test_django_query_implements_protocol(self):
+        """Django QueryParser implements QueryParserProtocol."""
+        request_mock = create_mock_request("name=alice&status__exact=active")
+        parser = DjangoQueryParams(request_mock)
+        assert isinstance(parser, QueryParserProtocol)
